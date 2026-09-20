5979647992a4ff863b9ebfdcab75d82fb71ae02d
Menelaus1997 <vantu7mer7men@gmail.com>
2026-09-20 22:33:32 +0300

    Update WindowFrame.tsx

--- src/WindowFrame.tsx
+++ src/WindowFrame.tsx
@@ -1,25 +1,38 @@
+import React from 'react';
+
+interface WindowFrameProps {
+  children: React.ReactNode;
+}
+
 export const WindowFrame: React.FC<WindowFrameProps> = ({ children }) => {
   return (
     <div style={{
+      position: 'fixed',
+      top: 0,
+      left: 0,
       width: '100vw',
       height: '100vh',
       margin: 0,
+      padding: 0,
       backgroundColor: '#ffffff',
       display: 'flex',
       flexDirection: 'column',
-      overflow: 'hidden', // <-- ОБОВ'ЯЗКОВО має бути hidden, щоб вікно не скролилось вбік
+      overflow: 'hidden',
       boxSizing: 'border-box',
     }}>
       <div style={{ 
         flex: 1, 
+        minHeight: 0, // Обов'язково для коректного скролу у flex-контейнері
         overflowY: 'auto', 
-        overflowX: 'hidden', // <-- ЗАБОРОНЯЄМО горизонтальний скрол для всього вмісту додатку
+        overflowX: 'hidden', 
         width: '100%', 
-        height: '100%',
-        boxSizing: 'border-box'
+        boxSizing: 'border-box',
+        position: 'relative'
       }}>
         {children}
       </div>
     </div>
   );
 };
+
+export default WindowFrame;
